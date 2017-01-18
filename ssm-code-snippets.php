b164634e13febc6e85fbf5a30bd098909651fbf7
Rich Staats <rich@secretstache.com>
2017-01-18 16:13:26 -0700

    Removes featured image from admin column

--- ssm-code-snippets.php
+++ ssm-code-snippets.php
@@ -9,7 +9,7 @@
  * Plugin Name: SSM Code Snippets
  * Plugin URI:  http://secretstache.com
  * Description: Enables a Code Snippet Custom Post Type.
- * Version:     0.1.0
+ * Version:     0.1.1
  * Author:      Secret Stache Media
  * Author URI:  http://secretstache.com
  * Text Domain: ssm-code-snippets
